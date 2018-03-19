75ed58e1b1bdcc0b1ec1bb7f9bc573bc33ca0dfe
RAM KRISHNA <abhinandanmay11@gmail.com>
2018-03-20 03:33:35 +0530

    Adding Bootstrap and some more features
    Basically I change the front end look. And add some more features like login and sign up. I add login and sign up features basically which have to modify a little bit.

--- homepage.php
+++ homepage.php
@@ -3,13 +3,37 @@
 	<head>
 		<title>Blood Bank HomePage</title>
 		<link rel="stylesheet" type="text/css" href="home.css"/>					<!-- Links the css file -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">		<!-- For fitting in all devices -->
+		<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">		 For fitting in all devices -->
+		<meta charset="utf-8">
+		 <meta name="viewport" content="width=device-width, initial-scale=1">
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  	</head>
 	<body>
-		<h1 style="text-align:center;color:red;"><em>Welcome To Online Blood Bank Portal</em></h1>		<!--title Of the Project in emphasized-->
-		<img class="home1" src="homeimg1.jpg" alt="Blood is Life">					<!--Sample Image Added-->
-		<a class="home1" href="homepage.html"><h1 class="home1">Home</h1></a>		<!--Links for Home -->
-		<a class="home1" href="login.html"><h1 class="home1">Login</h1></a>			<!--Link for Login --> 
-		<a class="home1" href="contact.html"><h1 class="home1">Contact Us</h1></a>	<!--Link for Contact us --> 
+		<!--<h1 style="text-align:center;color:red;"><em>Welcome To Online Blood Bank Portal</em></h1>		<!--title Of the Project in emphasized-->
+		<h1 style="text-align:center;color:red;"><em>Welcome To Online Blood Bank Portal</em></h1>
+		<img class="home1" src="homeimg1.jpg" alt="Blood is Life">					<!--Sample Image Added--> 
+		<div class="container">
+	  <div class = "section">
+	  <div class="btn-group btn-group-lg btn-group-justified">
+	    <div class="btn-group">
+	      <a href="homepage.php" button type="button" class="btn btn-primary">Home</button></a>
+	    </div>
+	    <div class="btn-group">
+	      <button type="button" class="btn btn-primary">About Us</button>
+	    </div>
+	    <div class="btn-group">
+	      <a href="rhlogin.php" button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span>Login</button></a>	<!--For login It will open into a login link -->
+	    </div>
+	    <div class="btn-group">
+	      <a href="rhsignup.php" button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-user"></span>Sign Up</button></a>	<!--For sign Up It will open into a sign up form -->
+	    </div>
+	    <div class="btn-group">
+	      <a href="contact.php" class="btn btn-primary">Contact Us</a>		<!--For contacting -->
+	    </div>
+	    <!--<a href="#" class="btn btn-primary">Apple</a> -->
+	  </div>    
+	</div>
 	</body>
 </html>
