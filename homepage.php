521ef446c2fea7c081edd76920dcbf4b59771c10
RAM KRISHNA <abhinandanmay11@gmail.com>
2018-03-30 01:57:20 +0530

    Add code for footer in homepage

--- homepage.php
+++ homepage.php
@@ -34,5 +34,105 @@
 		</div>
 	  </div>    
 	</div>
+	<div id="footer">
+  		<div id="footer_content">
+  			<div class="footer_item">
+  				<p class="footer_heading">
+  					Donate Blood				<!-- Footer Item We can add any link here-->
+  				</p>
+  				<ul class="footer_list">
+  					<li>
+  						<a href="">Blood Donation</a>		<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Blood Donation FAQs</a>	<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Types of Blood Donations</a>		<!-- Have to add any link here-->
+  					</li>
+  				</ul>
+  				<p class="footer_heading">
+  					About Blood
+  				</p>
+  				<ul class="footer_list">
+  					<li>
+  						<a href="">What is Blood?</a>			<!--Have to add the wipedia link -->
+  					</li>
+  					<li>
+  						<a href="">Blood is Always Needed</a>	<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">How Blood Saves Life</a>		<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Facts & Figures</a>			<!-- Have to add any link here-->
+  					</li>
+  				</ul>
+  			</div>
+  			<div class="footer_item">
+  				<p class="footer_heading">
+  				Get Involved
+  				</p>
+  				<ul class="footer_list">
+  					<li>
+  						<a href="">Plan a Blood Drive</a>		<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Blood Donation Programs</a>	<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  					<br/>
+  					</li>
+  				</ul>
+  				<p class="footer_heading">
+  					About Us
+  				</p>
+  				<ul class="footer_list">
+  					<li>
+  						<a href="">What We Do</a>				<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Our Programmes</a>			<!-- Have to add any link here-->
+  					</li>
+  				</ul>
+  			</div>
+  			<div class="footer_item">
+  			<p class="footer_heading">
+  				Tools & Resources
+  			</p>
+  			<ul class="footer_list">
+  				<li>
+  					<a href="">Publications</a>					<!-- Have to add any link here-->
+  				</li>
+  				<li>
+  					<a href="">Blog</a>							<!-- Have to add any link here-->
+  				</li>
+  				<li>
+  					<a href="">Contact Us</a>					<!-- Have to add any link here-->
+  				</li>
+  			</ul>
+  			</div>
+  			<div class="footer_item">
+  				<p class="footer_heading1">   </p>
+  			</div>
+  			<div class="footer_item_info">
+  				<h3>Online blood bank</h3>
+  				<ul class="footer_list">
+  					<li>Developed and maintained by:</li>
+  					<li>Vivek Verma</li>
+  					<li>Prasad Kshirsagar</li>
+  					<li>Komal Chugh</li>
+  					<li>Ram Krishna</li>
+  				</ul>
+  			</div>
+  		</div>
+		<div class="clear"></div>
+			<div id="footer_bottom">
+				<div class="wrapper">
+					<p>This site has been made under CSP203 Project</p>
+				</div>
+			</div>
+		</div>
+	</div>									<!--End of footer -->
 	</body>
 </html>
