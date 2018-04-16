9a83d681a51d8da06359ffeaa9bb072be5cecabe
RAM KRISHNA <abhinandanmay11@gmail.com>
2018-04-16 13:10:01 +0530

    Adding receiver files

---
+++ receiver.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Receiver Page</title>
+		<link rel="stylesheet" type="text/css" href="receiver.css"/>					<!-- Links the css file -->
+		<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">		 For fitting in all devices -->
+		<meta charset="utf-8">
+		 <meta name="viewport" content="width=device-width, initial-scale=1">
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 	</head>
+	<body>
+	<!-- 
+
+		<h1 style="text-align:center;color:red;"><em>Welcome To Blood Connect Web App</em></h1>
+		<img class="home1" src="homeimg1.jpg" alt="Blood is Life">					<!--Sample Image Added-->
+		<div id="container" style="white-space:nowrap">
+      <div>
+		    <div class="inline-block">
+		        <img class="home1" src="homeimg1.jpg" alt="Blood is Life"/>
+		    </div>
+
+		    <div class="inline-block"> 
+		        <h1 style="text-align:center;color:red;"><em>Welcome To Blood Connect Web App</em></h1>
+		    </div>
+      </div>
+		</div> 
+		<div class="container">
+		<div class="page-header">
+	  <div class = "section">
+	  <div class="btn-group btn-group-lg btn-group-justified">
+	    <div class="btn-group">
+	      <a href="homepage.php" button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-home"></span>Home</button></a>
+	    </div>
+	    <div class="btn-group">
+	      <a href="about.php" type="button" class="btn btn-primary">About Us</a>
+	    </div>
+      <div class="btn-group">
+        <a href="#" button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-user"></span>Hello User</button></a>  <!--For sign Up It will open into a sign up form -->
+      </div>
+    <div class="btn-group">
+        <a href="reciever.php" class="btn btn-primary">Request Blood</a>    <!--For contacting -->
+    </div>
+    <div class="btn-group">
+        <a href="contact.php" class="btn btn-primary">Contact Us</a>    <!--For contacting -->
+    </div>
+      <div class="btn-group">
+	      <a href="rhlogout.php" button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span>Log Out</button></a>	<!--For login It will open into a login link -->
+	    </div>
+	    
+	  </div>    
+	</div>
+  </div>
+  <h2>Blood Types</h2>
+  <hr>
+  <p>Generally hospitals have following type of bloods.</p>
+  <ul>
+    <li>A+</li>
+    <li>A-</li>
+    <li>B+</li>
+    <li>B-</li>
+    <li>AB+</li>
+    <li>AB-</li>
+    <li>O+</li>
+    <li>O-</li>
+  </ul>
+
+  <h2>Request Blood</h2>
+  <hr>
+  <p>You can find the blood <a href="#" style="color: red;">here.</a></p>
+
+	
+  </div>
+	<!-- Adding footer Have to modify-->
+	<div id="footer">
+  		<div id="footer_content">
+  			<div class="footer_item">
+  				<p class="footer_heading">
+  					Donate Blood				<!-- Footer Item We can add any link here-->
+  				</p>
+  				<ul class="footer_list">
+  					<li>
+  						<a href="">Blood Donation</a>		<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Blood Donation FAQs</a>	<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Types of Blood Donations</a>		<!-- Have to add any link here-->
+  					</li>
+  				</ul>
+  				<p class="footer_heading">
+  					About Blood
+  				</p>
+  				<ul class="footer_list">
+  					<li>
+  						<a href="">What is Blood?</a>			<!--Have to add the wipedia link -->
+  					</li>
+  					<li>
+  						<a href="">Blood is Always Needed</a>	<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">How Blood Saves Life</a>		<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Facts & Figures</a>			<!-- Have to add any link here-->
+  					</li>
+  				</ul>
+  			</div>
+  			<div class="footer_item">
+  				<p class="footer_heading">
+  				Get Involved
+  				</p>
+  				<ul class="footer_list">
+  					<li>
+  						<a href="">Plan a Blood Drive</a>		<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Blood Donation Programs</a>	<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  					<br/>
+  					</li>
+  				</ul>
+  				<p class="footer_heading">
+  					About Us
+  				</p>
+  				<ul class="footer_list">
+  					<li>
+  						<a href="">What We Do</a>				<!-- Have to add any link here-->
+  					</li>
+  					<li>
+  						<a href="">Our Programmes</a>			<!-- Have to add any link here-->
+  					</li>
+  				</ul>
+  			</div>
+  			<div class="footer_item">
+  			<p class="footer_heading">
+  				Tools & Resources
+  			</p>
+  			<ul class="footer_list">
+  				<li>
+  					<a href="">Publications</a>					<!-- Have to add any link here-->
+  				</li>
+  				<li>
+  					<a href="">Blog</a>							<!-- Have to add any link here-->
+  				</li>
+  				<li>
+  					<a href="">Contact Us</a>					<!-- Have to add any link here-->
+  				</li>
+  			</ul>
+  			</div>
+  			<div class="footer_item">
+  				<p class="footer_heading1">   </p>
+  			</div>
+  			<div class="footer_item_info">
+  				<h3>Online blood bank</h3>
+  				<ul class="footer_list">
+  					<li>Developed and maintained by:</li>
+  					<li>Vivek Verma</li>
+  					<li>Komal Chugh</li>
+  					<li>Ram Krishna</li>
+            <li>Prasad Kshirsagar</li>
+  				</ul>
+  			</div>
+  		</div>
+		<div class="clear"></div>
+			<div id="footer_bottom">
+				<div class="wrapper">
+					<p>This site has been made under CSP203 Project</p>
+				</div>
+			</div>
+		</div>
+	</div>									<!--End of footer -->
+	</body>
+</html>
