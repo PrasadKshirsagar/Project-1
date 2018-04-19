e86d34d12481c96117f91751f1a2c87fd3b40702
KomalChugh <35837993+KomalChugh@users.noreply.github.com>
2018-04-19 20:25:27 +0530

    Modified it to add link to map

--- kdistance.php
+++ kdistance.php
@@ -24,7 +24,7 @@
 	if ($result->num_rows > 0) {
 	// output data of each row
 	while($row = $result->fetch_assoc()) {
-	    echo "Address: ".$row["address"]." &nbsp &nbsp &nbsp &nbsp Distance in km: ".$row["distance"]."<br>";
+	    echo "<html><a target=\"_blank\" href=\"kmap.php?lat=".$latitude."&long=".$longitude."&add=".$row["address"]."\"> Address: ".$row["address"]."</a></html>"." &nbsp &nbsp &nbsp &nbsp Distance in km: ".$row["distance"]."<br>";
 	}
 	} else {
 	echo "0 results";
