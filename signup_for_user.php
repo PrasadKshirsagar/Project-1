6c381cd0481f935cfeb3df43fc9df6b482833093
PrasadKshirsagar <prasad23kshirsagar@gmail.com>
2018-04-23 00:00:18 +0530

    Modified sign up of user file (Added location par)

--- signup_for_user.php
+++ signup_for_user.php
@@ -90,12 +90,82 @@ if($result1->num_rows > 0)
     }
     
 }
+?>
+ <?php 
+// function to geocode address, it will return false if unable to geocode address
+function geocode($address){
+ 
+    // url encode the address
+    $address = urlencode($address);
+     
+    // google map geocode api url
+    $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyDzsn-4wZV91omAtAEogIe2xyKxEcFafCk";
+ 
+    // get the json response
+    $resp_json = file_get_contents($url);
+     
+    // decode the json
+    $resp = json_decode($resp_json, true);
+ 
+    // response status will be 'OK', if able to geocode given address 
+    if($resp['status']=='OK'){
+ 
+        // get the important data
+        $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
+        $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
+        $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+         
+        // verify if data is complete
+        if($lati && $longi && $formatted_address){
+         
+            // put the data in the array
+            $data_arr = array();            
+             
+            array_push(
+                $data_arr, 
+                    $lati, 
+                    $longi, 
+                    $formatted_address
+                );
+             
+            return $data_arr;
+             
+        }else{
+            return false;
+        }
+         
+    }
+ 
+    else{
+        echo "<strong>ERROR: {$resp['status']}</strong>";
+        return false;
+    }
+}
+?>
+<?php
+if($_POST){
+ 
+    // get latitude, longitude and formatted address
+    $data_arr = geocode($_POST['address']);
+ 
+    // if able to geocode the address
+    if($data_arr){
+         
+        $latitude = $data_arr[0];
+        $longitude = $data_arr[1];
+        $formatted_address = $data_arr[2];
+   
+    // if unable to geocode the address
+    }else{
+        echo "No map found.";
+    }
+}
        
        
       // echo 'outside2';
      
        
-       $query3 = "INSERT INTO address(address,user_id) VALUES ('$address', '$num')";
+       $query3 = "INSERT INTO address(address,user_id,latitude,longitude) VALUES ('$address', '$num','$latitude','$longitude')";
       $r3 = $conn->query($query3);  
      // echo $r3;
       
@@ -106,7 +176,7 @@ if($result1->num_rows > 0)
    	               // echo 'inside';
       		        $_SESSION["username"] = $user;
 			$_SESSION["password"] = $pwd1;
-       			header("Location: welcome.php?login=success"); 
+       			header("Location: user.php?login=success"); 
     
    	 } 
    	 else
